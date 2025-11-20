7e2cfb09d749919ccfa67b7a6f5be3d96b95ea06
hassanmehmood-fol <hassan.mehmood@folium.ai>
2025-11-20 17:00:16 +0500

    Run Migration of Ingredient Model

--- core/models.py
+++ core/models.py
@@ -50,6 +50,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag', related_name='recipes', blank=True)
+    ingredients=models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title        
@@ -58,6 +59,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
+        
         on_delete=models.CASCADE,
         related_name="tags"
     )
